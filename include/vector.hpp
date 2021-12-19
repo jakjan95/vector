9c61fe9ee8aeb8208b59ec25214040faba14a442
jakjan95 <jakubjanusz95@gmail.com>
2021-12-19 09:31:43 +0100

    system build with tests prepared

---
+++ include/vector.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace my_vec {
+
+template <typename T>
+class vector {
+};
+}
---
+++ tests/vector.ut.cpp
@@ -0,0 +1,6 @@
+#include "gtest/gtest.h"
+
+TEST(Empty, TrueShouldBeTrue)
+{
+    EXPECT_TRUE(true);
+}
